f47d41884b192adaf3ab95ae26e3c80aca2b9910
Furentsu <eugeniofranzoso@gmail.com>
2022-01-28 10:33:12 +0100

    Created answer's section

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,24 @@ function App() {
     <div className="app">
 
       {/* Question's section ( LEFT ) */}
-      <div className="questions">
+      <div className="question-section">
 
         {/* Question's count */}
         <div className="question-count">
-          <span>{ currentQuestion + 1 }</span> / {currentQuestion.length}
+          <span>{ currentQuestion + 1 }</span> / {survey.length}
         </div>
 
         {/* Question's text */}
         <div className="question-text">
           {survey[currentQuestion].questionText}
         </div>
-        
+
+      {/* Answer's section ( RIGHT ) */}
+        <div className="answer-section">
+          {survey[currentQuestion].answerOptions.map((answer) => {
+            return <button>{answer.answerText}</button>
+          })}
+        </div>
       </div>
     </div>
   );
