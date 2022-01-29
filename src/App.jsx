5df75c9a8a6c85c067e945f74ca21d158eaba1e4
Furentsu <eugeniofranzoso@gmail.com>
2022-01-29 14:17:20 +0100

    Implemented basic score and results display

--- src/App.jsx
+++ src/App.jsx
@@ -4,38 +4,60 @@ import {survey} from "./survey.js"
 function App() {
 
   const [currentQuestion, setCurrentQuestion] = useState(0)
+  const [score, setScore] = useState(0)
+  const [showResults, setShowResults] = useState(false)
 
-  function handleClick() {
-    setCurrentQuestion(currentQuestion + 1)
+  function handleClick(correctAnswer) {
+    if(correctAnswer) {
+      setScore(score + 1)
+    }
+    if ((currentQuestion + 1) < survey.length) {
+      setCurrentQuestion(currentQuestion + 1)
+    } else {
+      setShowResults(!showResults)
+    }
   }
 
   return (
     <div className="app">
 
-      {/* Question's section ( LEFT ) */}
-      <div className="question-section">
-
-        <div className="questions">
-
-          {/* Question's count */}
-          <div className="question-count">
-            <span>Question { currentQuestion + 1 }</span> / {survey.length}
+      {
+        showResults ? (
+          <div className="results-section">
+            You scored {score} out of {survey.length}
           </div>
+        ) : (
 
-          {/* Question's text */}
-          <div className="question-text">
-            {survey[currentQuestion].questionText}
-          </div>
-        </div>
-        
+          <>
+          
+            {/* Question's section ( LEFT ) */}
+            <div className="question-section">
 
-        {/* Answer's section ( RIGHT ) */}
-        <div className="answer-section">
-        {survey[currentQuestion].answerOptions.map((answer, index) => {
-            return <button className="answer" onClick={handleClick}>{answer.answerText}</button>
-          })}
-        </div>
-      </div>
+              <div className="questions">
+
+                {/* Question's count */}
+                <div className="question-count">
+                  <span>Question { currentQuestion + 1 }</span> / {survey.length}
+                </div>
+
+                {/* Question's text */}
+                <div className="question-text">
+                  {survey[currentQuestion].questionText}
+                </div>
+              </div>
+
+
+              {/* Answer's section ( RIGHT ) */}
+              <div className="answer-section">
+              {survey[currentQuestion].answerOptions.map((answer) => {
+                  return <button className="answer" onClick={() => handleClick(answer.isCorrect)}>{answer.answerText}</button>
+                })}
+              </div>
+            </div>
+
+          </>
+      )}
+
     </div>
   );
 }
